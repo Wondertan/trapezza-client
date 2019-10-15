9243208998b8308dbcba551c390d264cef249d41
Viktor.Markvardt <viktor.markvardt@gmail.com>
2019-10-15 18:27:16 +0300

    Added menu pages regarding current restaurant

---
+++ lib/pages/MyBill.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyBill extends StatefulWidget {
+  MyBillState createState(){
+    return MyBillState();
+  }
+}
+
+class MyBillState extends State<MyBill> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+    );
+  }
+}
---
+++ lib/pages/Participants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Participants extends StatefulWidget {
+  ParticipantsState createState(){
+    return ParticipantsState();
+  }
+}
+
+class ParticipantsState extends State<Participants> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+    );
+  }
+}
