04b5fc0efe73337a5e4ccdaedf231a8743ae69ce
Viktor.Markvardt <viktor.markvardt@gmail.com>
2019-10-15 03:38:44 +0300

    Added handling errors

--- trapezza_client_app/lib/pages/Join.dart
+++ trapezza_client_app/lib/pages/Join.dart
@@ -5,7 +5,7 @@ import 'package:trapezza_client_app/graphql/GraphqlConfiguration.dart';
 import 'package:trapezza_client_app/graphql/QueryMutation.dart';
 
 class Join extends StatefulWidget {
-  JoinState createState(){
+  JoinState createState() {
     return JoinState();
   }
 }
@@ -15,7 +15,7 @@ class JoinState extends State<Join> {
 
   String _qrCodeData = "";
 
-  String scanQrCode(){
+  String scanQrCode() {
     Future<String> futureString = new QRCodeReader()
         .setAutoFocusIntervalInMs(200) // default 5000
         .setForceAutoFocus(true) // default false
@@ -23,7 +23,7 @@ class JoinState extends State<Join> {
         .setHandlePermissions(true) // default true
         .setExecuteAfterPermissionGranted(true) // default true
         .scan();
-    
+
     futureString.then((value) {
       print("qr-code value: $value");
 
@@ -38,8 +38,6 @@ class JoinState extends State<Join> {
   @override
   void initState() {
     super.initState();
-
-    //checkSession();
   }
 
   void checkSession() async {
@@ -47,17 +45,22 @@ class JoinState extends State<Join> {
     GraphQLClient _client = graphQLConfiguration.clientToQuery();
     QueryResult result = await _client.mutate(
       MutationOptions(
-        document: queryMutation.checkSession("xx8ew", "1234"),
+        document: queryMutation.checkSession("xx8ew1", "1234"),
       ),
     );
 
-    if (!result.hasErrors) {
+    if (result.hasErrors) {
+
+      for(GraphQLError error in result.errors) {
+        print(error.toString());
+      }
+    } else {
       bool isSessionValid = result.data["addClient"];
 
       print('session status: $isSessionValid');
     }
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -76,15 +79,15 @@ class JoinState extends State<Join> {
                     textColor: Colors.white,
                     splashColor: Colors.blueGrey,
                     onPressed: scanQrCode,
-                    child: const Text('START SCAN')
-                ),
-              )
-              ,
+                    child: const Text('START SCAN')),
+              ),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: Text("Session code: $_qrCodeData", textAlign: TextAlign.center,),
-              )
-              ,
+                child: Text(
+                  "Session code: $_qrCodeData",
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ],
           ),
         ));
