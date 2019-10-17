5f03d838a5a49be7e6682963418399845bdba3e9
Viktor.Markvardt <viktor.markvardt@gmail.com>
2019-10-17 17:32:56 +0300

    Some improvements of tabBar

--- lib/pages/CurrentRestaurant.dart
+++ lib/pages/CurrentRestaurant.dart
@@ -10,151 +10,185 @@ class CurrentRestaurant extends StatefulWidget {
   }
 }
 
-class CurrentRestaurantState extends State<CurrentRestaurant> {
+class CurrentRestaurantState extends State<CurrentRestaurant> with SingleTickerProviderStateMixin {
+
+  TabController _tabController;
 
   @override
   void initState() {
+    _tabController = TabController(
+      length: 3,
+      vsync: this,
+    );
     super.initState();
   }
 
   @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: 100,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: Container(
-                    height: 100,
-                    color: Colors.transparent,
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          padding: new EdgeInsets.symmetric(horizontal: 8.0, vertical: 5.0),
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            'Best place in town',
-                            textAlign: TextAlign.center,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(fontSize: 18.0),
-                            maxLines: 2),
-                        ),
-                        Container(
-                          padding: new EdgeInsets.symmetric(horizontal: 8.0),
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                              'Location 1, Odessa, UA',
+    return Column(
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Container(
+                height: 100,
+                color: Colors.transparent,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: new EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0),
+                      alignment: Alignment.topLeft,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                              'Best place in town',
                               textAlign: TextAlign.center,
                               overflow: TextOverflow.ellipsis,
-                              style: TextStyle(fontSize: 14.0),
+                              style: TextStyle(fontSize: 18.0),
                               maxLines: 2),
-                        ),
-                        Container(
-                          padding: new EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
+                          Row(
                             children: <Widget>[
                               Icon(
-                                  Icons.star,
-                                  color: Colors.yellow,
-                                  size: 24.0
+                                  Icons.attach_money,
+                                  color: Colors.black,
+                                  size: 20
                               ),
                               Icon(
-                                  Icons.star,
-                                  color: Colors.yellow,
-                                  size: 24.0
+                                  Icons.attach_money,
+                                  color: Colors.black,
+                                  size: 20
                               ),
                               Icon(
-                                Icons.star,
-                                color: Colors.yellow,
-                                size: 24.0,
-                              ),
-                              Icon(
-                                Icons.star,
-                                color: Colors.yellow,
-                                size: 24.0,
-                              ),
-                              Icon(
-                                Icons.star,
-                                color: Colors.yellow,
-                                size: 24.0,
-                              ),
-                              Container(
-                                padding: new EdgeInsets.symmetric(horizontal: 8.0),
-                                alignment: Alignment.topLeft,
-                                child: Text(
-                                    '(5 reviews)',
-                                    textAlign: TextAlign.center,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(fontSize: 14.0),
-                                    maxLines: 1),
-                              ),
+                                  Icons.attach_money,
+                                  color: Colors.black,
+                                  size: 20
+                              )
                             ],
-                          ),
-                        )
-                      ],
+                          )
+                        ],
+                      )
                     ),
-                  ),
+                    Container(
+                      padding: new EdgeInsets.symmetric(horizontal: 8.0),
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                          'Location 1, Odessa, UA',
+                          textAlign: TextAlign.center,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(fontSize: 14.0),
+                          maxLines: 2),
+                    ),
+                    Container(
+                      padding: new EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 24.0
+                          ),
+                          Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 24.0
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 24.0,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 24.0,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 24.0,
+                          ),
+                          Container(
+                            padding: new EdgeInsets.symmetric(horizontal: 8.0),
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                                '(5 reviews)',
+                                textAlign: TextAlign.center,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(fontSize: 14.0),
+                                maxLines: 1),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
-
-                Container(
-                  width: 100,
-                  height: 100,
-                  child: Center(
-                    child: Container(
-                      width: 100,
-                      height: 100,
-                      margin: new EdgeInsets.symmetric(horizontal: 8.0),
-                      child: RawMaterialButton(
-                        fillColor: Colors.deepOrangeAccent,
-                        child: Text(
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              margin: new EdgeInsets.symmetric(
+                  horizontal: 5.0, vertical: 5.0),
+              child: Center(
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    margin: new EdgeInsets.symmetric(horizontal: 8.0),
+                    child: RawMaterialButton(
+                      fillColor: Colors.deepOrangeAccent,
+                      child: Text(
                           'Request waiter',
                           textAlign: TextAlign.center,
                           overflow: TextOverflow.ellipsis,
                           style: TextStyle(fontSize: 16.0),
                           maxLines: 2),
-                        shape: CircleBorder(),
-                        elevation: 0.0,
-                        onPressed: (){},
-                      ),
-                    )
-                  ),
-                )
-              ],
-            )
-          ),
-
-          Expanded(
-            child: Container(
-              child: DefaultTabController(
-                length: 3,
-                child: Scaffold(
-                  appBar: AppBar(
-                      bottom: TabBar(
-                        tabs: [
-                          Tab(text: 'Menu'),
-                          Tab(text: 'My bill'),
-                          Tab(text: 'Participants')
-                        ],
-                      )
-                  ),
-                  body: TabBarView(
-                    children: [
-                      Menu(),
-                      MyBill(),
-                      Participants()
-                    ],
-                  ),
-                ),
+                      shape: CircleBorder(),
+                      elevation: 0.0,
+                      onPressed: () {},
+                    ),
+                  )
               ),
+            )
+          ],
+        ),
+        Container(
+          color: Color(0xFFF3F3F3),
+          child: TabBar(
+            controller: _tabController,
+            indicatorColor: Colors.redAccent,
+            labelColor: Colors.redAccent,
+            unselectedLabelStyle: TextStyle(
+                color: Colors.green,
+                decorationColor: Colors.black
             ),
+            //isScrollable: true,
+            tabs: [
+              Tab(text: "Menu"),
+              Tab(text: "My Bill"),
+              Tab(text: "Participants")
+            ],
+          )
+        ),
+        Expanded(
+          child: TabBarView(
+            controller: _tabController,
+            children: [
+              Menu(),
+              MyBill(),
+              Participants()
+            ],
           ),
-        ],
-      )
+        )
+      ],
     );
   }
 }
