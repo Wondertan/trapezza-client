561aafa7ce3dac32c8e32fd3e3ed29c888729688
Viktor.Markvardt <viktor.markvardt@gmail.com>
2019-10-20 23:47:25 +0300

    Implemented class "CustomColors"

---
+++ lib/properties/CustomColors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class CustomColors {
+  static Color cherry = Color(0xFFEB2B54);
+  static Color lightGrey = Color(0xFFF8F8F8);
+  static Color grey = Color(0xFFEEEEEE);
+  static Color yellow = Color(0xFFFAC917);
+  static Color green = Color(0xFF619E7A);
+}
