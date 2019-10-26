63d903cb119b87d282c6a0427fe74c5db6e89bfb
Viktor.Markvardt <viktor.markvardt@gmail.com>
2019-10-26 17:27:09 +0300

    Minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/services.dart';
-import 'package:trapezza_client_app/app/AppState.dart';
-import 'package:trapezza_client_app/graphql/GraphqlConfiguration.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import "package:graphql_flutter/graphql_flutter.dart";
+import 'package:trapezza_client_app/graphql/GraphqlConfiguration.dart';
+import 'package:trapezza_client_app/utils/AppInitializer.dart';
 
 GraphQLConfiguration graphQLConfiguration = GraphQLConfiguration();
 
 void main() {
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
-    runApp(
-      GraphQLProvider(
-        client: graphQLConfiguration.client,
-        child: CacheProvider(child: App()),
-      ),
-    );
-  });
+        runApp(
+          GraphQLProvider(
+            client: graphQLConfiguration.client,
+            child: CacheProvider(child: AppInitializer()),
+          ),
+        );
+      }
+  );
 }
