41c7a646071765c2da1d76c84376f6cf34d66de8
Viktor.Markvardt <viktor.markvardt@gmail.com>
2019-10-27 06:06:34 +0300

    Minor changes

--- lib/pages/Friends.dart
+++ lib/pages/Friends.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trapezza_client_app/properties/CustomColors.dart';
 
 class Friends extends StatefulWidget {
   FriendsState createState(){
@@ -11,7 +12,16 @@ class FriendsState extends State<Friends> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.lightBlueAccent,
+      color: Colors.white,
+      child: Center(
+        child: Text(
+            'Coming soon... : )',
+            style: TextStyle(
+              fontSize: 24,
+              color: CustomColors.cherry
+            )
+        ),
+      ),
     );
   }
 }
