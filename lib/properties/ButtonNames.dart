ff6968f57020d07831e354c116db13a621e7d60f
Viktor.Markvardt <viktor.markvardt@gmail.com>
2019-10-29 22:49:28 +0300

    Added class ButtonNames

---
+++ lib/properties/ButtonNames.dart
@@ -0,0 +1,12 @@
+class ButtonNames {
+  // bottom navigation bar
+  static final String events = "Events";
+  static final String restaurants = "Restaurants";
+  static final String friends = "Friends";
+  static final String profile = "Profile";
+  static final String checkout = "Checkout";
+  static final String join = "Join";
+
+  // join page
+  static final String requestWaiter = "RequestWaiter";
+}
