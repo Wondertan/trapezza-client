2ef320607cf87bb209bf67f3f9f1274cad98197e
Viktor.Markvardt <viktor.markvardt@gmail.com>
2019-11-02 15:49:49 +0300

    Minor improvements

--- lib/popups/CommonPopupContent.dart
+++ lib/popups/CommonPopupContent.dart
@@ -18,6 +18,13 @@ class _CommonPopupContentState extends State<CommonPopupContent> {
   }
 
   @override
+  void setState(fn) {
+    if(this.mounted){
+      super.setState(fn);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       child: widget.content,
