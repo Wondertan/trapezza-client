f65bd28387138f760cd843e89133a00b7e996ca4
Viktor.Markvardt <viktor.markvardt@gmail.com>
2019-11-03 22:50:39 +0300

    Added colors

--- lib/properties/CustomColors.dart
+++ lib/properties/CustomColors.dart
@@ -7,5 +7,6 @@ class CustomColors {
   static Color yellow = Color(0xFFFAC917);
   static Color green = Color(0xFF619E7A);
   static Color darkGrey = Color(0xFF979797);
+  static Color whiteGrey = Color(0xFFBDBDBD);
   static Color black = Color(0xFF333333);
 }
